c890dbde654e39997d75792903a28bdc2c2bd681
dongle94 <sdy1029@gmail.com>
2023-06-13 14:48:13 +0900

    yolov5 warmup 까지 작업 완료 - 이어서 nms 작업 필요

--- detectors/yolov5_pt.py
+++ detectors/yolov5_pt.py
@@ -1,3 +1,4 @@
+import os
 import math
 import numpy as np
 import torch
@@ -8,6 +9,7 @@ class YoloDetector(nn.Module):
     def __init__(self, weight='yolov5s.pt', device=torch.device('cpu'), fp16=False, fuse=True):
         super().__init__()
 
+        self.device = self.select_device(device)
         self.cuda = torch.cuda.is_available() and device
         self.fp16 = fp16
         model = attempt_load(weight, device=device, inplace=True, fuse=fuse)
@@ -37,12 +39,25 @@ class YoloDetector(nn.Module):
         self.forward(im)  # warmup
         print("-- Yolov5 Detector warmup --")
 
+    def select_device(self, device=''):
+        device = str(device).strip().lower().replace('cuda', '').replace('none', '')
+        cpu = device == 'cpu'
+        if cpu:
+            os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+        elif device:  # non-cpu device requested
+            os.environ[
+                'CUDA_VISIBLE_DEVICES'] = device  # set environment variable - must be before assert is_available()
+
+        if not cpu and torch.cuda.is_available():
+            device = device if device else '0'
+            arg = 'cuda:0'
+        else:
+            arg = 'cpu'
+
+        return torch.device(arg)
 
 def attempt_load(weight, device=None, inplace=True, fuse=True):
 
-    # model = Ensemble()
-    if device not in ["cpu", None]:
-        device = f"cuda:{device}"
     ckpt = torch.load(str(weight), map_location='cpu')
     ckpt = (ckpt.get('ema') or ckpt['model']).to(device).float()
 
@@ -53,10 +68,22 @@ def attempt_load(weight, device=None, inplace=True, fuse=True):
         ckpt.names = dict(enumerate(ckpt.names))  # convert to dict
     ckpt = ckpt.fuse().eval() if fuse and hasattr(ckpt, 'fuse') else ckpt.eval()
 
+    from models.yolo import Detect, Model
+    # Module compatibility updates
+    for m in ckpt.modules():
+        t = type(m)
+        if t in (nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6, nn.SiLU, Detect, Model):
+            m.inplace = inplace  # torch 1.7.0 compatibility
+            if t is Detect and not isinstance(m.anchor_grid, list):
+                delattr(m, 'anchor_grid')
+                setattr(m, 'anchor_grid', [torch.zeros(1)] * m.nl)
+        elif t is nn.Upsample and not hasattr(m, 'recompute_scale_factor'):
+            m.recompute_scale_factor = None  # torch 1.11.0 compatibility
+
     # return model
     return ckpt
 
 if __name__ == "__main__":
-    model = YoloDetector(weight='../weights/yolov5n.pt')
-
+    model = YoloDetector(weight='../weights/yolov5n.pt', device='cpu')
+    model.warmup()
     # print(dir(model), type(model))
