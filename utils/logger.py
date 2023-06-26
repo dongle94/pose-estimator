7bcf37d7bceb8f9ff2d2caccafc30bfd52b86285
dongle94 <sdy1029@gmail.com>
2023-06-26 14:46:59 +0900

    feat: logger 세분화 - 콘솔로그와 파일로그   - 각 옵션을 True / False 로 On/off 가능   - 파일로그는 TimedRotatingFile 핸들러로써 시간대별로 로그 저장기능     - 파일 사이즈 옵션은 시간 기반이라 사용하지 않음     - 파일의 갯수(저장기간)과 저장간격을 옵션으로 설정 - 사용법   - 기본적으로 cfg를 파라미터로 로거를 init하고 get_logger를 호출하여 로거 인스턴스를 확보   - name을 명시 하여 get_logger 호출 시 해당 name 단위의 모듈로거 획

--- utils/logger.py
+++ utils/logger.py
@@ -1,32 +1,97 @@
 import logging
+from logging import StreamHandler
+from logging.handlers import TimedRotatingFileHandler
 import os
 
 
 logger = None
 
 
-def init_logger(name="", filename=None, loglevel='INFO'):
-    # if not exist log dir, mkdir
-    logdir = os.path.dirname(filename)
-    if not os.path.exists(logdir):
-        os.makedirs(logdir)
-
+def init_logger(cfg=None, name="empty_logger", filename="", loglevel="debug"):
+    # LOG FORMATTING
     # https://docs.python.org/ko/3.8/library/logging.html#logrecord-attributes
     log_format = "[%(asctime)s]-[%(levelname)s]-[%(name)s]-[%(module)s](%(process)d): %(message)s"
     date_format = '%Y-%m-%d %H:%M:%S'
 
-    logging.basicConfig(filename=filename,
-                        filemode='a',
-                        format=log_format,
-                        datefmt=date_format,
-                        level=loglevel)
-    logging.info("logging Start")
+    if cfg is not None:
+        # LOGGER NAME
+        name = cfg.LOGGER_NAME if cfg.LOGGER_NAME else "logger"
+        _logger = logging.getLogger(name)
 
+        # LOG LEVEL
+        log_level = cfg.LOG_LEVEL if cfg.LOG_LEVEL else "DEBUG"
+        _logger.setLevel(log_level)
+
+        # LOG CONSOLE
+        if cfg.CONSOLE_LOG is True:
+            _handler = StreamHandler()
+            _handler.setLevel(log_level)
+
+            # logger formatting
+            formatter = logging.Formatter(log_format)
+            _handler.setFormatter(formatter)
+            _logger.addHandler(_handler)
+
+        # LOG FILE
+        if cfg.FILE_LOG is True:
+            filename = os.path.join(cfg.LOG_FILE_DIR, cfg.LOGGER_NAME + '.log')
+            logdir = os.path.dirname(filename)
+            if not os.path.exists(logdir):
+                os.makedirs(logdir)
+
+            when = cfg.LOG_FILE_ROTATE_TIME if hasattr(cfg, "LOG_FILE_ROTATE_TIME") else "D"
+            interval = cfg.LOG_FILE_ROTATE_INTERVAL if hasattr(cfg, "LOG_FILE_ROTATE_INTERVAL") else 1
+            _handler = TimedRotatingFileHandler(
+                filename=filename,
+                when=when,
+                interval=interval,
+                backupCount=cfg.LOG_FILE_COUNTER,
+                encoding='utf8'
+            )
+            _handler.setLevel(log_level)
+
+            # logger formatting
+            formatter = logging.Formatter(log_format)
+            _handler.setFormatter(formatter)
+            _logger.addHandler(_handler)
+
+    else:       # cfg is None
+        _logger = logging.getLogger(name)
+        _logger.setLevel(loglevel)
+
+        # CONSOLE LOGGER
+        _handler = StreamHandler()
+        _handler.setLevel(loglevel)
+        formatter = logging.Formatter(log_format)
+        _handler.setFormatter(formatter)
+        _logger.addHandler(_handler)
+
+        # FILE LOGGER
+        filename = os.path.join('./log', name + '.log')
+        logdir = os.path.dirname(filename)
+        if not os.path.exists(logdir):
+            os.makedirs(logdir)
+
+        _handler = TimedRotatingFileHandler(
+            filename=filename,
+            when="D",
+            interval=1,
+            backupCount=10,
+            encoding='utf8'
+        )
+        _handler.setLevel(loglevel)
+        formatter = logging.Formatter(log_format)
+        _handler.setFormatter(formatter)
+        _logger.addHandler(_handler)
+
+    _logger.info("Start Main logger")
     global logger
-    logger = logging
+    logger = _logger
 
 
 def get_logger(name=None):
-    global logger
-    if logger is not None:
+    if name is None:
+        global logger
         return logger
+    else:
+        return logging.getLogger(name)
