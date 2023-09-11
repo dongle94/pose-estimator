d96493e3777e5d8f7575a66ebf1de18c8f608ce6
dongle94 <sdy1029@gmail.com>
2023-09-11 16:52:24 +0900

    Feat: config for yolo - 객체감지용 설정 추가

--- utils/config.py
+++ utils/config.py
@@ -20,6 +20,11 @@ _C.IMG_SIZE = 640
 _C.DET_MODEL_TYPE = ""
 _C.DET_MODEL_PATH = ""
 _C.HALF = False
+_C.OBJ_CLASSES = None
+_C.CONF_THRES = 0.5
+_C.NMS_IOU = 0.45
+_C.AGNOSTIC_NMS = True
+_C.MAX_DET = 100
 
 
 # Keypoint Detector
