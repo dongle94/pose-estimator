4f1bc0dce5f20d2c3ec35be70d9b1fde92d8a2ca
dongle94 <sdy1029@gmail.com>
2024-10-24 19:35:21 +0900

    Feat: ViTPose utils
    - util.py, torch_utils.py
    - gpu 옵션 스트링으로 torch.device 인스턴스 리턴
    - 모델 사이즈 스트링으로 vitpose config 파일 딕셔너리형태로 리턴

--- core/vitpose/vit_utils/util.py
+++ core/vitpose/vit_utils/util.py
@@ -1,7 +1,23 @@
+from importlib import import_module
 import warnings
 
 
-def resize(input,
+MODEL_ABBR_MAP = {
+    's': 'small',
+    'b': 'base',
+    'l': 'large',
+    'h': 'huge'
+}
+
+
+def dyn_model_import(dataset: str, model: str):
+    config_name = f'cfg.ViTPose_{dataset}'
+    imp = import_module(config_name)
+    model = f'model_{MODEL_ABBR_MAP[model]}'
+    return getattr(imp, model)
+
+
+def resize(inputs,
            size=None,
            scale_factor=None,
            mode='nearest',
@@ -9,7 +25,7 @@ def resize(input,
            warning=True):
     if warning:
         if size is not None and align_corners:
-            input_h, input_w = int(input.shape[0]), int(input.shape[1])
+            input_h, input_w = int(inputs.shape[0]), int(inputs.shape[1])
             output_h, output_w = int(size[0]), int(size[1])
             if output_h > input_h or output_w > output_h:
                 if ((output_h > 1 and output_w > 1 and input_h > 1
